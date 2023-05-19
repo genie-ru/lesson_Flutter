b60b0c6ab0d5dbd225084994360c01ac86cd3208
genie-ru <genie.zgmf987@gmail.com>
2023-05-20 07:19:54 +0900

    add firebase

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_application_1/main_model.dart';
- 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async{
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const ProviderScope(child: MyApp()));
 }
  
